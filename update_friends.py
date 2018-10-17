936c8baa27b5f3d44d596f6d80209d0fba6a2bc6
VladimirI <Vladimir.Ivanov@aquantia.com>
2018-10-17 22:11:44 +0300

    some bugs fixed

--- update_friends.py
+++ update_friends.py
@@ -5,45 +5,62 @@ import time
 import json
 
 timeFormatBase = '%Y-%m-%d-%H-%M-%S'
-timeFormatFriends =  timeFormatBase + '.txt'
+timeFormatFriends = timeFormatBase + '.txt'
 timeFormatFriendsFull = timeFormatBase + '_Full.txt'
 timeFormatFollowers = timeFormatBase + '_followers.txt'
 
 targetUserId = 0
-vk_session = vk_api.VkApi('***', '****')
+
+fd = open("json.auth", "r")
+auth = json.load(fd)
+
+vk_session = vk_api.VkApi(auth["login"], auth["password"]);
 vk_session.auth(token_only=True)
 vk = vk_session.get_api()
 
-friends = vk.friends.get(user_id=targetUserId,order='name')
+fd = open("targets.json", "r")
+targets = json.load(fd)
+
+targetUserId = targets["id"][0];
+
+friends_ids = vk.friends.get(user_id=targetUserId)
 ts = time.time();
 st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriends)
 file = open(st, 'w+')
-file.write(json.dumps(friends)) 
+file.write(json.dumps(friends_ids))
 
-friends = vk.friends.get(user_id=targetUserId,fields='name')
+friends = vk.friends.get(user_id=targetUserId, fields='name')
 ts = time.time();
-st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriends)
+st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriendsFull)
 file = open(st, 'w+')
 file.write(json.dumps(friends))
 
-followers = vk.users.get(user_id=targetUserId,fields='name') 
+followers = vk.users.getFollowers(user_id=targetUserId, fields='name')
 ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFollowers)
 file = open(st, 'w+')
 file.write(json.dumps(followers))
 
-dir = os.listdir()
+dir = os.listdir("./")
 times = []
 
 for filename in dir:
-	try:
-		times.append(datetime.datetime.strptime(str(file.name), timeFormatFriends))
-	except:
-		continue
-youngest_entry = max(times)
-youngest_file = youngest_entry.strftime(timeFormatFriends)
-fd=open(youngest_file,"r")
+    flag = False
+    try:
+		filenameParsed = datetime.datetime.strptime(str(filename), timeFormatFriends)
+    except:
+        flag = True
+    if not flag:
+        times.append(filenameParsed)
 
-#data = json.load(fd)
-#print(set(data.item) & set(friends_ids.item))
+try:
+    youngest_entry = max(times)
+    youngest_file = youngest_entry.strftime(timeFormatFriends)
+    fd=open(youngest_file,"r")
 
+    data = json.load(fd)
+
+    print(set(data["items"]).union(set(friends_ids["items"])))
+
+finally:
+    exit(0)
