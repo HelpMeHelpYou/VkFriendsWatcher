48ddbb199597526fff50d1cf5759d2b3f8cb8094
Vladimir Ivanov <polar_winter@mail.ru>
2018-10-18 16:02:43 +0300

    updated

--- update_friends.py
+++ update_friends.py
@@ -11,56 +11,84 @@ timeFormatFollowers = timeFormatBase + '_followers.txt'
 
 targetUserId = 0
 
-fd = open("json.auth", "r")
-auth = json.load(fd)
+def get_vk_api():
+	fd = open("json.auth", "r")
+	auth = json.load(fd)
+	vk_session = vk_api.VkApi(auth["login"], auth["password"]);
+	vk_session.auth(token_only=True)
+	vk = vk_session.get_api()
+	return vk
 
-vk_session = vk_api.VkApi(auth["login"], auth["password"]);
-vk_session.auth(token_only=True)
-vk = vk_session.get_api()
 
-fd = open("targets.json", "r")
-targets = json.load(fd)
+def get_targets():
+	fd = open("targets.json", "r")
+	targets = json.load(fd)
+	return targets["id"]
 
-targetUserId = targets["id"][0];
+vk = get_vk_api()
+targetUserId = get_targets()[0];
+
 
 friends_ids = vk.friends.get(user_id=targetUserId)
-ts = time.time();
-st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriends)
-file = open(st, 'w+')
-file.write(json.dumps(friends_ids))
 
-friends = vk.friends.get(user_id=targetUserId, fields='name')
-ts = time.time();
-st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriendsFull)
-file = open(st, 'w+')
-file.write(json.dumps(friends))
 
-followers = vk.users.getFollowers(user_id=targetUserId, fields='name')
-ts = time.time()
-st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFollowers)
-file = open(st, 'w+')
-file.write(json.dumps(followers))
+def update_data(vk,tragetUserId):
+	friends_ids = vk.friends.get(user_id=targetUserId)
+	ts = time.time();
+	st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriends)
+	file = open(st, 'w+')
+	file.write(json.dumps(friends_ids))
+
+	friends = vk.friends.get(user_id=targetUserId, fields='name')
+	ts = time.time();
+	st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriendsFull)
+	file = open(st, 'w+')
+	file.write(json.dumps(friends))
+
+	followers = vk.users.getFollowers(user_id=targetUserId, fields='name')
+	ts = time.time()
+	st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFollowers)
+	file = open(st, 'w+')
+	file.write(json.dumps(followers))
+	return friends_ids
+
+def print_diff ( set1, set2):
+        print ("deleted")
+        print(set1 - set2)
+        print ("added")
+        print (set2 - set1)
+
 
 dir = os.listdir("./")
 times = []
-
+filenames= []
 for filename in dir:
     flag = False
     try:
-		filenameParsed = datetime.datetime.strptime(str(filename), timeFormatFriends)
+        filenameParsed = datetime.datetime.strptime(str(filename), timeFormatFriends)
     except:
         flag = True
     if not flag:
         times.append(filenameParsed)
+        filenames.append(filename)
 
-try:
-    youngest_entry = max(times)
-    youngest_file = youngest_entry.strftime(timeFormatFriends)
-    fd=open(youngest_file,"r")
+times.sort()
+for i in range(len(times)):
+
+    filename = times[i].strftime(timeFormatFriends)
+    fd=open(filename,"r")
 
     data = json.load(fd)
+    fd.close()
 
-    print(set(data["items"]).union(set(friends_ids["items"])))
+    if (i+1 == len(times)):
+    	print_diff( set(data["items"]) , set(friends_ids["items"]))
+    else:
+        filename2 = times[i+1].strftime(timeFormatFriends)
+        fd = open (filename2,"r")
+        data2 = json.load(fd)
+        fd.close()
+        print (filename2)
+        print_diff ( set(data["items"]), set(data2["items"]))
+    print ("")
 
-finally:
-    exit(0)
