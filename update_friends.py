63428be9b18e17fb1836c8f210b6922f1b8773d8
VladimirI <Vladimir.Ivanov@aquantia.com>
2018-10-18 16:43:02 +0300

    pep*

--- update_friends.py
+++ update_friends.py
@@ -12,57 +12,59 @@ timeFormatFollowers = timeFormatBase + '_followers.txt'
 targetUserId = 0
 
 def get_vk_api():
-	fd = open("json.auth", "r")
-	auth = json.load(fd)
-	vk_session = vk_api.VkApi(auth["login"], auth["password"]);
-	vk_session.auth(token_only=True)
-	vk = vk_session.get_api()
-	return vk
+    file_descriptor = open("json.auth", "r")
+    auth = json.load(file_descriptor)
+    vk_session = vk_api.VkApi(auth["login"], auth["password"]);
+    vk_session.auth(token_only=True)
+    vk = vk_session.get_api()
+    return vk
 
 
 def get_targets():
-	fd = open("targets.json", "r")
-	targets = json.load(fd)
-	return targets["id"]
+    file_descriptor = open("targets.json", "r")
+    targets = json.load(file_descriptor)
+    return targets["id"]
+
 
 vk = get_vk_api()
 targetUserId = get_targets()[0];
 
-
 friends_ids = vk.friends.get(user_id=targetUserId)
 
 
-def update_data(vk,tragetUserId):
-	friends_ids = vk.friends.get(user_id=targetUserId)
-	ts = time.time();
-	st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriends)
-	file = open(st, 'w+')
-	file.write(json.dumps(friends_ids))
+def update_data(vk_instance, traget_user_id):
+    friends_ids = vk_instance.friends.get(user_id=targetUserId)
+    current_time = time.time()
+    file_name = datetime.datetime.fromtimestamp(current_time).strftime(timeFormatFriends)
+    file_descriptor = open(file_name, 'w+')
+    file_descriptor.write(json.dumps(friends_ids))
+    file_descriptor.close()
 
-	friends = vk.friends.get(user_id=targetUserId, fields='name')
-	ts = time.time();
-	st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFriendsFull)
-	file = open(st, 'w+')
-	file.write(json.dumps(friends))
+    friends = vk_instance.friends.get(user_id=targetUserId, fields='name')
+    file_name = datetime.datetime.fromtimestamp(current_time).strftime(timeFormatFriendsFull)
+    file_descriptor = open(file_name, 'w+')
+    file_descriptor.write(json.dumps(friends))
+    file_descriptor.close()
 
-	followers = vk.users.getFollowers(user_id=targetUserId, fields='name')
-	ts = time.time()
-	st = datetime.datetime.fromtimestamp(ts).strftime(timeFormatFollowers)
-	file = open(st, 'w+')
-	file.write(json.dumps(followers))
-	return friends_ids
+    followers = vk_instance.users.getFollowers(user_id=targetUserId, fields='name')
+    st = datetime.datetime.fromtimestamp(current_time).strftime(timeFormatFollowers)
+    file_descriptor = open(st, 'w+')
+    file_descriptor.write(json.dumps(followers))
+    file_descriptor.close()
+    return friends_ids
 
-def print_diff ( set1, set2):
+
+def print_diff(set1, set2):
         print ("deleted")
         print(set1 - set2)
         print ("added")
         print (set2 - set1)
 
 
-dir = os.listdir("./")
+current_dir = os.listdir("./")
 times = []
 filenames= []
-for filename in dir:
+for filename in current_dir:
     flag = False
     try:
         filenameParsed = datetime.datetime.strptime(str(filename), timeFormatFriends)
@@ -76,19 +78,19 @@ times.sort()
 for i in range(len(times)):
 
     filename = times[i].strftime(timeFormatFriends)
-    fd=open(filename,"r")
+    fd = open(filename, "r")
 
     data = json.load(fd)
     fd.close()
 
-    if (i+1 == len(times)):
-    	print_diff( set(data["items"]) , set(friends_ids["items"]))
+    if i+1 == len(times):
+        print_diff(set(data["items"]), set(friends_ids["items"]))
     else:
         filename2 = times[i+1].strftime(timeFormatFriends)
-        fd = open (filename2,"r")
+        fd = open(filename2, "r")
         data2 = json.load(fd)
         fd.close()
         print (filename2)
-        print_diff ( set(data["items"]), set(data2["items"]))
+        print_diff(set(data["items"]), set(data2["items"]))
     print ("")
 
